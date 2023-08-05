d55f2a4e7898fbbdf17ed22c3748cef34f63a863
Magnus Olsson <magols@live.com>
2023-08-05 11:01:27 +0200

    Server components enables in template

--- BlazorTemplate/Program.cs
+++ BlazorTemplate/Program.cs
@@ -3,7 +3,7 @@ using BlazorTemplate;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents();
+builder.Services.AddRazorComponents().AddServerComponents();
 
 var app = builder.Build();
 
